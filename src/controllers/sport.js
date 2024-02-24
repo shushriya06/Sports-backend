b0c3767532e5331e654dca8ce1fe3f2ea9c309b2
Shushriya Swarnkar <swarnkar@adobe.com>
2024-02-24 15:16:13 +0530

    Problem 2

--- src/controllers/sport.js
+++ src/controllers/sport.js
@@ -4,14 +4,14 @@ const getAllSportsToursAndMatches = async () => {
     const matches = await Sport.getAllSportsToursAndMatches();
     const res = {};
     matches.forEach(match => {
-        const { sportName, tourName, matchName } = match;
+        const { sportName, tourName, matchName, matchId, matchStartTime, matchFormat } = match;
         if (!res[sportName]) {
             res[sportName] = {};
         }
         if (!res[sportName][tourName]) {
             res[sportName][tourName] = [];
         }
-        res[sportName][tourName].push(matchName);
+        res[sportName][tourName].push({matchName, matchId, matchStartTime, matchFormat});
     });
     return res;
 }
--- src/models/sport.js
+++ src/models/sport.js
@@ -1,7 +1,7 @@
 const mysql = require('../lib/mysql');
 
 const getAllSportsToursAndMatches = async () => {
-    const statement = 'select s.name as sportName, t.name as tourName, m.name as matchName ' +
+    const statement = 'select s.name as sportName, t.name as tourName, m.name as matchName , m.id as matchId, m.startTime as matchStartTime, m.format as matchFormat ' +
         'from matches m inner join tours t on m.tourId = t.id ' +
         'inner join sports s on t.sportId = s.id';
     const parameters = [];
